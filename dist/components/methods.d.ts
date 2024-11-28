40fa67c49380e27932cb8d32034578beec6239fd
Njato <m.pdfb002.njato.optimumsolutions@gmail.com>
2024-11-28 10:29:54 +0300

    fix:option issue

--- dist/components/methods.d.ts
+++ dist/components/methods.d.ts
@@ -1,2 +1,4 @@
+import { CanvasSnapOptions } from "./hooks";
 export declare const copyBase64ImageToClipboard: (base64String: string, mimeType?: string) => Promise<void>;
+export declare const mergeSnapOptions: (target: CanvasSnapOptions, source: CanvasSnapOptions) => CanvasSnapOptions;
 //# sourceMappingURL=methods.d.ts.map
